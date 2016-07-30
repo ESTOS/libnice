4c7561d262bece892e226b75f798c51a6bd1c884
Kyrylo Polezhaiev <polezhaiev@ukr.net>
2016-07-30 03:49:07 +0300

    Add #ifdef/#endif guards to prevent TCP_NODELAY macro redeclaration

--- socket/tcp-passive.c
+++ socket/tcp-passive.c
@@ -53,7 +53,9 @@
 /* FIXME: This should be defined in gio/gnetworking.h, which we should include;
  * but we cannot do that without refactoring.
  * (See: https://phabricator.freedesktop.org/D230). */
+#ifndef TCP_NODELAY
 #define TCP_NODELAY 1
+#endif
 
 typedef struct {
   GMainContext *context;
